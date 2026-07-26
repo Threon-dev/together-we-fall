74e1ffb83a05db88c04ef96139a509577a43e6a1
Threon <threondev@gmail.com>
2026-07-26 11:05:49 +0300

    Add: Simple enemy attack logic

--- Assets/_Project/Code/Player/PlayerController.cs
+++ Assets/_Project/Code/Player/PlayerController.cs
@@ -5,11 +5,32 @@ namespace _Project.Code.Player
 {
     public class PlayerController : NetworkBehaviour
     {
+        [SerializeField] private int maxHealth = 100;
         [SerializeField] private Transform shootingPoint;
         [SerializeField] private NetworkPrefabRef projectilePrefab;
         [SerializeField] private float fireRate = 0.3f;
+        
+        [Networked] public int CurrentHealth { get; set; }
 
         private TickTimer _fireCooldownTimer;
+
+        public override void Spawned()
+        {
+            CurrentHealth = maxHealth;
+        }
+
+        public void TakeDamage(int damage)
+        {
+            if (!Object.HasStateAuthority) return;
+            
+            Debug.Log("Player taking damage");
+            
+            CurrentHealth -= damage;
+            if (CurrentHealth <= 0)
+            {
+                Debug.Log("Player died");
+            }
+        }
         
         public override void FixedUpdateNetwork()
         {
